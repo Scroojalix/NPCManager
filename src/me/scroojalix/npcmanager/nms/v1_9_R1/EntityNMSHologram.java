aad0b897903a19c9c82d4c633cf767d48f0257ea
BuildTools <scroojalixyt@gmail.com>
2020-10-30 18:28:27 +1100

    Added 1.9 support

---
+++ src/me/scroojalix/npcmanager/nms/v1_9_R1/EntityNMSHologram.java
@@ -0,0 +1,28 @@
+package me.scroojalix.npcmanager.nms.v1_9_R1;
+
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_9_R1.CraftWorld;
+
+import me.scroojalix.npcmanager.nms.interfaces.NMSHologram;
+import net.minecraft.server.v1_9_R1.EntityArmorStand;
+
+public class EntityNMSHologram extends EntityArmorStand implements NMSHologram {
+
+	public EntityNMSHologram(Location loc, String text) {
+		super(((CraftWorld)loc.getWorld()).getHandle());
+		setCustomName(text);
+		setCustomNameVisible(getCustomName() != null && !getCustomName().isEmpty());
+	    setInvisible(true);
+	    setSmall(true);
+		setArms(false);
+		setGravity(false);
+	    setBasePlate(true);
+	    setMarker(true);
+	    this.collides = false;
+		setPosition(loc.getX(), loc.getY(), loc.getZ());
+	}
+
+	public EntityArmorStand getEntity() {
+		return this;
+	}
+}
