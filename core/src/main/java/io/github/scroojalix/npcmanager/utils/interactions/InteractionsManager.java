ec585326a3ba07ebf37d9c82bd1dfdea26181417
Scroojalix <scroojalixyt@gmail.com>
2020-12-29 19:39:16 +1100

    Allow underscores in custom interactEvent names

--- core/src/main/java/io/github/scroojalix/npcmanager/utils/interactions/InteractionsManager.java
+++ core/src/main/java/io/github/scroojalix/npcmanager/utils/interactions/InteractionsManager.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.bukkit.plugin.Plugin;
 
 import io.github.scroojalix.npcmanager.NPCMain;
+import io.github.scroojalix.npcmanager.utils.PluginUtils;
 
 public class InteractionsManager {
 
@@ -24,7 +25,7 @@ public class InteractionsManager {
 		if (interactEvent.getInteractionName() == null) {
 			NPCMain.instance.getLogger().warning("Could not register interaction from "+plugin.getName()+". It's name is null.");
 			return;
-		} else if (interactEvent.getInteractionName().matches("^[a-zA-Z0-9]*$")) {
+		} else if (PluginUtils.isAlphanumeric(interactEvent.getInteractionName())) {
 			String name = interactEvent.getInteractionName();
 			interactEvents.put(name, interactEvent);
 		} else {
