f7affda6e714c6c1a0609c39a452cebe93e2c4f3
Scroojalix <scroojalixyt@gmail.com>
2021-04-05 00:28:40 +1000

    🔨 code refactor: added global dependencies

--- core/src/main/java/io/github/scroojalix/npcmanager/utils/dependencies/DependencyRegistry.java
+++ core/src/main/java/io/github/scroojalix/npcmanager/utils/dependencies/DependencyRegistry.java
@@ -1,22 +1,26 @@
 package io.github.scroojalix.npcmanager.utils.dependencies;
 
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
 
-import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.ListMultimap;
+import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.collect.SetMultimap;
 
 import io.github.scroojalix.npcmanager.utils.storage.StorageType;
 
 public class DependencyRegistry {
 
-    private static final ListMultimap<StorageType, Dependency> STORAGE_DEPENDENCIES = ImmutableListMultimap.<StorageType, Dependency>builder()
+    private static final SetMultimap<StorageType, Dependency> STORAGE_DEPENDENCIES = ImmutableSetMultimap.<StorageType, Dependency>builder()
     .putAll(StorageType.MONGODB, Dependency.MONGODB_DRIVER)
     .build();
 
     public static Set<Dependency> resolveStorageDependencies(StorageType storageType) {
-        Set<Dependency> dependencies = new LinkedHashSet<>();
-        dependencies.addAll(STORAGE_DEPENDENCIES.get(storageType));
+        return STORAGE_DEPENDENCIES.get(storageType);
+    }
+
+    public static Set<Dependency> getGlobalDependencies() {
+        Set<Dependency> dependencies = new HashSet<Dependency>();
+        dependencies.add(Dependency.ASM);
         return dependencies;
     }
 
