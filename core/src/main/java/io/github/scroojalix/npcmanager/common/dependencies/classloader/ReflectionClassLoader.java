6a8f9318479ec40fbd8ddae61071f6f878fc514f
Scroojalix <scroojalixyt@gmail.com>
2022-12-18 02:17:45 +1100

    Fixed accidental reversion of classpath fix

--- core/src/main/java/io/github/scroojalix/npcmanager/common/dependencies/classloader/ReflectionClassLoader.java
+++ core/src/main/java/io/github/scroojalix/npcmanager/common/dependencies/classloader/ReflectionClassLoader.java
@@ -11,33 +11,8 @@ import io.github.scroojalix.npcmanager.NPCMain;
 public class ReflectionClassLoader {
 
     @SuppressWarnings("Guava")
-    private static final Supplier<Method> ADD_URL_METHOD = Suppliers.memoize(() -> {
-        // If on Java 9+, open the URLClassLoader module to this module
-        // so we can access its API via reflection without producing a warning.
-        try {
-            openUrlClassLoaderModule();
-        } catch (Exception e) {
-            e.printStackTrace();
-            // ignore exception - will throw on Java 8 since the Module classes don't exist
-        }
-        try {
-            Method addUrlMethod = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-            addUrlMethod.setAccessible(true);
-            return addUrlMethod;
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    });
-    private final URLClassLoader classLoader;
-
-    public ReflectionClassLoader(NPCMain main) throws IllegalStateException {
-        ClassLoader classLoader = main.getClass().getClassLoader();
-        if (classLoader instanceof URLClassLoader) {
-            this.classLoader = (URLClassLoader) classLoader;
-        } else {
-            throw new IllegalStateException("ClassLoader is not instance of URLClassLoader");
-        }
-    }
+    private static final Supplier<URLClassLoaderAccess> URL_INJECTOR = Suppliers.memoize(
+            () -> URLClassLoaderAccess.create((URLClassLoader) NPCMain.class.getClassLoader()));
 
     public static void addJarToClasspath(Path file) {
         try {
@@ -45,24 +20,5 @@ public class ReflectionClassLoader {
         } catch (Exception e) {
             throw new RuntimeException("Unable to load a dependency.", e);
         }
-    }
-
-    @SuppressWarnings("JavaReflectionMemberAccess")
-    private static void openUrlClassLoaderModule() throws Exception {
-        Class<?> moduleClass = Class.forName("java.lang.Module");
-        Method getModuleMethod = Class.class.getMethod("getModule");
-        Method addOpensMethod = moduleClass.getMethod("addOpens", String.class, moduleClass);
-
-        Object urlClassLoaderModule = getModuleMethod.invoke(URLClassLoader.class);
-        Object thisModule = getModuleMethod.invoke(ReflectionClassLoader.class);
-
-        //TODO get base module
-        Object baseModule = getModuleMethod.invoke(Class.forName("java.base"));
-
-        NPCMain.instance.getLogger().info(thisModule.toString());
-
-        addOpensMethod.invoke(baseModule, "java.net", thisModule);
-
-        addOpensMethod.invoke(urlClassLoaderModule, URLClassLoader.class.getPackage().getName(), thisModule);
     }
 }
