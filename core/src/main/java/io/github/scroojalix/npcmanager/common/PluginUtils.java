7d5457892372dde1c9174e069cd3ac75b8ae9cdb
Owen <73511853+Scroojalix@users.noreply.github.com>
2022-12-26 22:03:00 +1100

    1.19-support (#22)

--- core/src/main/java/io/github/scroojalix/npcmanager/common/PluginUtils.java
+++ core/src/main/java/io/github/scroojalix/npcmanager/common/PluginUtils.java
@@ -133,7 +133,8 @@ public class PluginUtils {
 		v1_12_R1(false, true, 1), v1_13_R1(false, false, 2), v1_13_R2(false, false, 2),
 		v1_14_R1(false, false, 2), v1_15_R1(false, false, 2), v1_16_R1(true, false, 2),
 		v1_16_R2(true, false, 2), v1_16_R3(true, false, 2), v1_17_R1(true, false, 2),
-		v1_18_R1(true, false, 2), v1_18_R2(true, false, 2);
+		v1_18_R1(true, false, 2), v1_18_R2(true, false, 2), v1_19_R1(true, false, 2),
+		v1_19_R2(true, false, 2);
 
 		public final boolean hasHexSupport;
 		public final boolean usesDamageForColouredMaterials;
